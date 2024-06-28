d8c8fca350e8abde554faccb0928f240b9c66717
katfogy <katfogy@gmail.com>
2024-06-28 13:13:26 +0100

    short_code field added

--- database/migrations/2024_06_26_160738_add_short_code_to_programs_table.php
+++ database/migrations/2024_06_26_160738_add_short_code_to_programs_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('programs', function (Blueprint $table) {
-            //
+            $table->dropColumn('short_code');
         });
     }
 };
