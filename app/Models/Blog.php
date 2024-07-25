21df39c7eef7daa148357de9b14c86eb5095e79f
katfogy <katfogy@gmail.com>
2024-07-25 16:25:48 +0100

    Blog features completed

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,13 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Blog extends Model
 {
     use HasFactory;
     protected $guarded = [];
 
-    public function category(){
-        return $this->belongsTo(BlogCategory::class);
-       }
+    public function category()
+    {
+        return $this->belongsTo(BlogCategory::class, 'category_id');
+    }
+       protected static function boot()
+    {
+        parent::boot();
+        static::saving(function ($blog) {
+            if ($blog->isDirty('title')) {
+                $blog->slug = Str::slug($blog->title);
+            }
+        });
+    }
 }
---
+++ resources/views/admin/update-category.blade.php
@@ -0,0 +1,70 @@
+@extends('admin.index')
+@section('slot')
+    @include('components.sweetalert')
+    <div class="page-wrapper">
+        <div class="page-content">
+            <!--breadcrumb-->
+            <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+                <div class="breadcrumb-title pe-3">Category</div>
+                <div class="ps-3">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb mb-0 p-0">
+                            <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                            </li>
+                            <li class="breadcrumb-item " aria-current="page">Category</li>
+                            <li class="breadcrumb-item active" aria-current="page">Update</li>
+                        </ol>
+                    </nav>
+                </div>
+                <div class="ms-auto">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-primary">List of Categories</button>
+                    </div>
+                </div>
+            </div>
+            <!--end breadcrumb-->
+            <h6 class="mb-0 text-uppercase">Update Category</h6>
+            <hr />
+            <div class="card">
+                <div class="card-body">
+                    <h6 class="mb-0 text-uppercase">Category</h6>
+                    <hr />
+                    
+                    <form action="{{ route('category.edit.store', $category->id) }}" enctype="multipart/form-data" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <label for="formFile" class="form-label">Category Title</label>
+                        <input class="form-control w-100 form-control-lg mb-3" name="title" type="text"
+                            value="{{ old('title', $category->title) }}"
+                            aria-label=".form-control-lg example">
+                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label for="text_color" class="form-label">Text Color</label>
+                                    <input class="form-control w-100 form-control-lg mb-3" name="text_color" type="color"
+                                        value="{{ old('text_color', $category->text_color) }}"
+                                        aria-label=".form-control-lg example">
+                                    <x-input-error :messages="$errors->get('text_color')" class="mt-2" />
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="bg_color" class="form-label">Background Color</label>
+                                    <input class="form-control w-100 form-control-lg mb-3" name="bg_color" type="color"
+                                        value="{{ old('bg_color', $category->bg_color) }}"
+                                        aria-label=".form-control-lg example">
+                                    <x-input-error :messages="$errors->get('bg_color')" class="mt-2" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <button class="btn btn-primary px-4">
+                            Update
+                            <i class="bx bx-right-arrow-alt ms-2"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
